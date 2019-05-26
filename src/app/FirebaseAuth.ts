48976f5aa54356b41fa9222f8bbf879323cfcecd
roniel06 <roniel06.rc@gmail.com>
2019-05-26 14:19:42 -0400

    r

--- src/app/FirebaseAuth.ts
+++ src/app/FirebaseAuth.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CREDENTIALS={
-    apiKey: "AIzaSyCePSbUkWU-ZjHuOBeSZazZ20UbctARBuA",
-    authDomain: "fir-crud-7eab7.firebaseapp.com",
-    databaseURL: "https://fir-crud-7eab7.firebaseio.com",
-    projectId: "fir-crud-7eab7",
-    storageBucket: "fir-crud-7eab7.appspot.com",
-    messagingSenderId: "427860454991",
-    appId: "1:427860454991:web:209330bc8045a745"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
